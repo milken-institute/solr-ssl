b989bded71a9f83759b4f43603d2d9620e307cbc
Tom Stovall <stovak@gmail.com>
2020-03-06 15:04:17 -0800

    Fixing issues with certs and default certs when not available

--- solr-proxy/docker-entrypoint.sh
+++ solr-proxy/docker-entrypoint.sh
@@ -2,5 +2,27 @@
 
 set -e
 
+export PASSWORD=""
+
+## if the actual certs don't exist, copy the self-signed ones so nginx can still start
+if [ -f "/etc/ssl/certs/cert.pem" ]; then
+    cp /etc/ssl/certs/cert.pem /etc/ssl/certs/reapbooster-cert.pem
+  else
+    openssl x509 -in /etc/ssl/certs/nginx-selfsigned.crt -out /etc/ssl/certs/reapbooster-cert.pem
+fi
+
+if [ -f "/etc/ssl/private/privkey.pem" ]; then
+    cp /etc/ssl/private/privkey.pem /etc/ssl/private/reapbooster-privkey.pem
+  else
+    openssl pkcs12 -export -in /etc/ssl/certs/nginx-selfsigned.crt \
+     -nodes \
+     -inkey /etc/ssl/private/nginx-selfsigned.key \
+     -out /etc/ssl/private/nginx-selfsigned.p12 \
+     -password pass:${PASSWORD}
+    openssl pkcs12 -in /etc/ssl/private/nginx-selfsigned.p12 \
+     -nodes \
+     -password pass:${PASSWORD} \
+     -out /etc/ssl/private/reapbooster-privkey.pem
+fi
 
 /usr/bin/supervisord
